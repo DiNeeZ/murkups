782b43e5eeb23ec18cf0f54baae31bee0eb597f3
DiNeez <dneez86@gmail.com>
2023-01-26 15:33:00 +0200

    2 projects finished

---
+++ analytics/README.md
@@ -0,0 +1,6 @@
+# Gulp - Gulp Starter Tamplate 2022
+
+Gulp version - 4.0.2
+
+# analytics
+# analytics
---
+++ analytics/src/js/_functions.js
@@ -0,0 +1,95 @@
+// This file is just a collection of prefabricated component connections.
+// It is recommended to create a separate file in the components folder and connect everything there
+
+// Determining the operating system on mobile
+// import { mobileCheck } from "./functions/mobile-check";
+// console.log(mobileCheck())
+
+// Screen Width Determination
+// import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
+// if (isDesktop()) {
+//   console.log('...')
+// }
+
+// Throttling functions
+// import { throttle } from './functions/throttle';
+// let yourFunc = () => { console.log('throttle') };
+// let func = throttle(yourFunc);
+// window.addEventListener('resize', func);
+
+// Fix fullscreen blocks
+// import './functions/fix-fullheight';
+
+// Burdger menu
+import { burger } from "./functions/burger";
+
+// Disable scrolling (do not forget to call the function)
+// import { disableScroll } from './functions/disable-scroll';
+
+// Enable scrolling (do not forget to call the function)
+// import { enableScroll } from './functions/enable-scroll';
+
+// Get site header height (do not forget to call the function)
+// import { getHeaderHeight } from './functions/header-height';
+
+// The custom scroll plugin
+// import 'simplebar';
+
+// The custom scroll plugin
+// import { createPopper, right} from '@popperjs/core';
+// createPopper(el, tooltip, {
+//   placement: 'right'
+// });
+
+// Swiper slider
+import Swiper, { Navigation, Pagination } from "swiper";
+Swiper.use([Navigation, Pagination]);
+const pricingSlider = new Swiper(".pricing-slider", {
+  initialSlide: 1,
+  slidesPerView: "auto",
+  slideActiveClass: "active",
+  centeredSlides: true,
+  breakpoints: {
+    576: {
+      slidesPerView: "auto",
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 80,
+      allowTouchMove: false,
+    },
+  },
+});
+
+// Animations on scroll
+// import AOS from 'aos';
+// AOS.init();
+
+// Block parallax on scroll
+// import Rellax from 'rellax';
+// const rellax = new Rellax('.rellax');
+
+// Smooth scrolling to anchors
+// import SmoothScroll from 'smooth-scroll';
+// const scroll = new SmoothScroll('a[href*="#"]');
+
+// swipe events on mobile
+// import 'swiped-events';
+// document.addEventListener('swiped', function(e) {
+//   console.log(e.target);
+//   console.log(e.detail);
+//   console.log(e.detail.dir);
+// });
+
+// import { validateForms } from './functions/validate-forms';
+// const rules1 = [...];
+
+// const afterForm = () => {
+//   console.log('Sending happened, here you can write any actions');
+// };
+
+// validateForms('.form-1', rules1, afterForm);
