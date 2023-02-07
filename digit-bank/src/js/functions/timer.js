f381f1b9188ea4955df60dd46a463eb6c24db741
DiNeez <dneez86@gmail.com>
2023-02-07 17:32:20 +0200

    about page for desktop is done

--- digit-bank/src/js/functions/timer.js
+++ digit-bank/src/js/functions/timer.js
@@ -1,28 +1,30 @@
 (function () {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+  if (document.querySelector(".timer")) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  const start = new Date("January 17, 2023 03:24:00");
+    const start = new Date("January 17, 2023 03:24:00");
 
-  setInterval(() => {
-    const now = new Date().getTime();
-    const distance = now - start;
+    setInterval(() => {
+      const now = new Date().getTime();
+      const distance = now - start;
 
-    const days = Math.floor(distance / day);
-    const hours = Math.floor((distance % day) / hour);
-    const minutes = Math.floor((distance % hour) / minute);
-    const seconds = Math.floor((distance % minute) / second);
+      const days = Math.floor(distance / day);
+      const hours = Math.floor((distance % day) / hour);
+      const minutes = Math.floor((distance % hour) / minute);
+      const seconds = Math.floor((distance % minute) / second);
 
-    const daysEl = document.getElementById("days");
-    const hoursEl = document.getElementById("hours");
-    const minutesEl = document.getElementById("minutes");
-    const secondsEl = document.getElementById("seconds");
+      const daysEl = document.getElementById("days");
+      const hoursEl = document.getElementById("hours");
+      const minutesEl = document.getElementById("minutes");
+      const secondsEl = document.getElementById("seconds");
 
-    if (daysEl.innerText != days) daysEl.innerText = days;
-    if (hoursEl.innerText != hours) hoursEl.innerText = hours;
-    if (minutesEl.innerText != minutes) minutesEl.innerText = minutes;
-    if (secondsEl.innerText != seconds) secondsEl.innerText = seconds;
-  }, 1000);
+      if (daysEl.innerText != days) daysEl.innerText = days;
+      if (hoursEl.innerText != hours) hoursEl.innerText = hours;
+      if (minutesEl.innerText != minutes) minutesEl.innerText = minutes;
+      if (secondsEl.innerText != seconds) secondsEl.innerText = seconds;
+    }, 1000);
+  }
 })();
