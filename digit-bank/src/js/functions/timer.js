53f7082d0c35eee4b9a0f80ccfc28008a9559c7e
DiNeeZ <dneez86@gmail.com>
2023-02-12 14:53:32 +0200

    Detail Service page is started

--- digit-bank/src/js/functions/timer.js
+++ digit-bank/src/js/functions/timer.js
@@ -5,9 +5,7 @@
     const hour = minute * 60;
     const day = hour * 24;
 
-    const start = new Date("January 17, 2023 03:24:00");
-
-    setInterval(() => {
+    function getDistanceFromNow(start) {
       const now = new Date().getTime();
       const distance = now - start;
 
@@ -16,10 +14,32 @@
       const minutes = Math.floor((distance % hour) / minute);
       const seconds = Math.floor((distance % minute) / second);
 
-      const daysEl = document.getElementById("days");
-      const hoursEl = document.getElementById("hours");
-      const minutesEl = document.getElementById("minutes");
-      const secondsEl = document.getElementById("seconds");
+      return {
+        days,
+        hours,
+        minutes,
+        seconds,
+      };
+    }
+
+    function initTimer(start) {
+      const { days, hours, minutes, seconds } = getDistanceFromNow(start );
+      daysEl.innerText = days;
+      hoursEl.innerText = hours;
+      minutesEl.innerText = minutes;
+      secondsEl.innerText = seconds;
+    }
+
+    const startTime = new Date("January 17, 2023 14:50  :00");
+    const daysEl = document.getElementById("days");
+    const hoursEl = document.getElementById("hours");
+    const minutesEl = document.getElementById("minutes");
+    const secondsEl = document.getElementById("seconds");
+
+    initTimer(startTime)
+
+    setInterval(() => {
+      const { days, hours, minutes, seconds } = getDistanceFromNow(startTime);
 
       if (daysEl.innerText != days) daysEl.innerText = days;
       if (hoursEl.innerText != hours) hoursEl.innerText = hours;
